43eed7f940fad269d59393ebfa47232a37d43034
park dae min <34418203+daeminn@users.noreply.github.com>
2023-02-04 17:59:14 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -3,12 +3,12 @@ $(window).ready(function () {
    let wH = window.innerHeight;
    const body = $("body");
    const hd = $('#osc-hd');
-   let hdH = hd.innerHeight;
    let gnbOpener = $('.gnb-open');
    let gnbCloser = $('.gnb-close');
    let gnbWrap = $('.gnb-wrap');
+   let depth1 = $('.depth1');
    let depth1a = $('.depth1>li>a')
-   let menuPullDown = $('.menu-open');
+   let menuPullDown = $('.all-menu-open');
    let menuCloser = $('.all-menu-close');
    let allMenu = $('#all-menu');
 
@@ -16,7 +16,6 @@ $(window).ready(function () {
 
    $(window).resize(function () {
       rwd();
-      reset();
    });
 
    function rwd() {
@@ -26,49 +25,81 @@ $(window).ready(function () {
          body.addClass("mo").removeClass("tb pc");
          $(".depth2").stop().slideUp();
          $(".section").addClass("fp-auto-height");
+         $(".develop, .business").css("width","inherit")      
+      
       } else if (wW >= 768 && wW < 1024) {
          body.addClass("tb").removeClass("mo pc");
+         $(".section").removeClass("fp-auto-height");
+         $("#osc-ft").addClass("fp-auto-height");
+         $(".develop, .business").css("width","50%")
       } else {
          body.addClass("pc").removeClass("mo tb");
-         
+         $(".section").removeClass("fp-auto-height");
+         $("#osc-ft").addClass("fp-auto-height");
+         $(".develop, .business").css("width","50%")
       }
-   }
+   }  
 
-   function reset() {
-      hdH = hd.height();
-      gnbReset();
-  }
+   //모바일 GNB 작동
+   $(".depth1>li").on({
+      "click": (function () {
+         if (body.hasClass("mo")||body.hasClass("tb")) {
+            $(this).children(".depth2").stop().slideToggle(300);
+            $(this).siblings().find(".depth2").stop().slideUp(300);
+         }
+      })
+   });
 
-  function gnbReset() {
-      if (body.hasClass("mo")||body.hasClass("tb")){
-      }
-  }
-  
-  
 
-//모바일 GNB 작동
-$(".depth1>li").on({
-   "click": (function () {
-       if (body.hasClass("mo")||body.hasClass("tb")) {
-           $(this).children(".depth2").stop().slideToggle(300);
-           $(this).siblings().find(".depth2").stop().slideUp(300);
-       }
-   })
-});
-//헤더 GNB 작동
+   //헤더 컬러변경
    hd.on({
-      "mouseenter": function () {
-         if (body.hasClass("pc") || body.hasClass("tb")) {
-            $(this).addClass("active");
+      "mouseenter": function(){
+         if (body.hasClass("pc") || body.hasClass("tb")){
+            gnbWrap.addClass("color");
          }
       },
-      "mouseleave": function () {
+      "mouseleave": function(){
+         if (body.hasClass("pc") || body.hasClass("tb")){
+            gnbWrap.removeClass("color");
+         }
+      }
+   })
+   
+   //PC GNB 활성화
+   depth1.on({
+      "mouseenter": function () {
          if (body.hasClass("pc") || body.hasClass("tb")) {
-            $(this).removeClass("active");
+            hd.addClass("active");
+            $('#fp-nav').css("opacity","0");
          }
       }
    });
-   
+   //PC GNB 비활성화
+   hd.on({
+      "mouseleave": function () {
+         if (body.hasClass("pc") || body.hasClass("tb")) {
+            hd.removeClass("active");
+            $('#fp-nav').css("opacity","1");
+         }
+      }
+   })
+   //메인화면 sec01 table-cell 이벤트
+   $(".develop, .business").on({
+      "mouseenter": function(){
+         if(body.hasClass("pc") || body.hasClass("tb")){
+            $(this).css("width","70%");
+            $(this).siblings().css("width","30%");
+         }
+      },
+      "mouseleave": function(){
+         if(body.hasClass("pc") || body.hasClass("tb")){
+            $(this).css("width","50%");
+            $(this).siblings().css("width","50%");
+         }
+      }
+   })
+
+   //모바일 GNB 열기
    gnbOpener.on({
       "click": function () {
          if (body.hasClass("mo")||body.hasClass("tb")) {
@@ -76,6 +107,8 @@ $(".depth1>li").on({
          }
       }
    });
+   
+   //모바일 GNB 닫기
    gnbCloser.on({
       "click": function () {
          if (body.hasClass("mo")||body.hasClass("tb")) { 
@@ -84,6 +117,8 @@ $(".depth1>li").on({
          }
       }
    });
+
+   //사이트맵 열기
    menuPullDown.on ({
       "click":function() {
          if (body.hasClass("pc")) {
@@ -92,6 +127,7 @@ $(".depth1>li").on({
          }
       }
    })
+   //사이트맵 닫기
    menuCloser.on ({
       "click":function() {
          if (body.hasClass("pc")) {
--- js/main.js
+++ js/main.js
@@ -1,61 +1,89 @@
 $(function(){
-  let wW = window.innerWidth;
-  let wH = window.innerHeight;
-  const body = $("body");
 
   const fpCon = $("#osc-main-container");
   let ftEl = $("#osc-ft").clone().addClass("section fp-auto-height");;
   fpCon.append(ftEl);
   fpCon.fullpage({
-    navigationTooltips:  ['01','02','03'],
+    navigationTooltips: ['01','02','03'],
     navigation: true,
     navigationPosition: 'right',
     scrollingSpeed: 1000,
     normalScrollElements: '.gnb-wrap, #all-menu',
     lazyLoading: true,
     showActiveTooltip:true,
-    scrollOverflow:true,
 
    });
 
-   rwd();
+   // rwd();
 
-   $(window).resize(function () {
-      rwd();
-      reset();
-   });
+   // $(window).resize(function () {
+   //    rwd();
+   // });
 
-   function rwd() {
-      wW = window.innerWidth;
-      wH = window.innerHeight;
-      if (wW < 768) {
-         body.addClass("mo").removeClass("tb pc");
-         $(".depth2").stop().slideUp();
-         $(".section").addClass("fp-auto-height");
-      } else if (wW >= 768 && wW < 1024) {
-         body.addClass("tb").removeClass("mo pc");
-         section01();
-      } else {
-         body.addClass("pc").removeClass("mo tb");
-         section01();
-      }
-   }
-   /* table cell 너비조절 */
-   function section01(){
-    let develop = $('.develop');
-    let business = $('.business');
-    let sec01Box = $('.sec01-box');
-    develop.mouseenter(function(){
-      $(this).css("width","70%");
-      business.css("width","30%");
-    })
-    business.mouseenter(function(){
-      $(this).css("width","70%");
-      develop.css("width","30%");
-    })
-    sec01Box.mouseleave(function(){
-      develop.css("width","50%");
-      business.css("width","50%");
-    })
-   }
+   // function rwd() {
+   //    wW = window.innerWidth;
+   //    wH = window.innerHeight;
+   //    if (wW < 768) {
+   //       body.addClass("mo").removeClass("tb pc");
+   //       $(".depth2").stop().slideUp();
+   //       $(".section").addClass("fp-auto-height");
+   //       $(".develop, .business").css("width","inherit")
+         
+   //    } else if (wW >= 768 && wW < 1024) {
+   //       body.addClass("tb").removeClass("mo pc");
+   //       $(".section").removeClass("fp-auto-height");
+   //       $("#osc-ft").addClass("fp-auto-height");
+   //       $(".develop, .business").css("width","50%")
+   //    } else {
+   //       body.addClass("pc").removeClass("mo tb");
+   //       $(".section").removeClass("fp-auto-height");
+   //       $("#osc-ft").addClass("fp-auto-height");
+   //       $(".develop, .business").css("width","50%")
+   //    }
+   // }
+
+   
+   // let develop = $('.develop');
+   // let business = $('.business');
+   // let sec01Box = $('.sec01-box');
+   // /* table cell 마우스엔터 */
+
+   // $(".develop, .business").on({
+   //    "mouseenter": function(){
+   //       if(body.hasClass("pc") || body.hasClass("tb")){
+   //          $(this).css("width","70%");
+   //          $(this).siblings().css("width","30%");
+   //       }
+   //    },
+   //    "mouseleave": function(){
+   //       if(body.hasClass("pc") || body.hasClass("tb")){
+   //          $(this).css("width","50%");
+   //          $(this).siblings().css("width","50%");
+   //       }
+   //    }
+   // })
+   // $('.sec01-box').on({
+   //    "mouseleave": function(){
+   //       if(body.hasClass("pc") || body.hasClass("tb")){
+   //          develop.css("width","50%");
+   //          business.css("width","50%");
+   //       }
+   //    }
+   // })
+   
+   // function section01(){
+    
+   //  develop.mouseenter(function(){
+   //    $(this).css("width","70%");
+   //    business.css("width","30%");
+   //  })
+   //  business.mouseenter(function(){
+   //    $(this).css("width","70%");
+   //    develop.css("width","30%");
+   //  })
+   //  sec01Box.mouseleave(function(){
+   //    develop.css("width","50%");
+   //    business.css("width","50%");
+   //  })
+   // }
  });
